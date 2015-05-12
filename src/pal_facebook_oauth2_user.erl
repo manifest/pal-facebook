6d5c2a9a1bcbb3edc4325b4ad6456d961bbcf58d
Andrei Nesterov <ae.nesterov@gmail.com>
2015-05-12 17:25:09 +0300

    added support of email and gender schema's props

--- src/pal_facebook_oauth2_user.erl
+++ src/pal_facebook_oauth2_user.erl
@@ -46,6 +46,8 @@
 -define(FIRST_NAME, <<"first_name">>).
 -define(LAST_NAME, <<"last_name">>).
 -define(LINK, <<"link">>).
+-define(EMAIL, <<"email">>).
+-define(GENDER, <<"gender">>).
 -define(FACEBOOK, <<"facebook">>).
 
 %% Types
@@ -88,6 +90,8 @@ uid(Data) ->
 info([{?NAME, Val}|T], M)       -> info(T, M#{name => Val});
 info([{?FIRST_NAME, Val}|T], M) -> info(T, M#{first_name => Val});
 info([{?LAST_NAME, Val}|T], M)  -> info(T, M#{last_name => Val});
+info([{?GENDER, Val}|T], M)     -> info(T, M#{gender => Val});
+info([{?EMAIL, Val}|T], M)      -> info(T, M#{email => Val});
 info([{?LINK, Val}|T], M)       -> info(T, M#{urls => maps:put(?FACEBOOK, Val, #{})});
 info([_|T], M)                  -> info(T, M);
 info([], M)                     -> M.
